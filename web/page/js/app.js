a40152ab708b6e985dbf43596bbcc0fae603917d
Brooks J Rady <b.j.rady@gmail.com>
2019-07-14 22:51:09 +0100

    The slickening

---
+++ web/page/js/app.js
@@ -0,0 +1,29 @@
+// Here be JS
+
+// Wait for the DOM to load before kicking things off
+document.addEventListener('DOMContentLoaded', function(event) {
+    var logo = `
+          .//.            .//.          
+          .//.            .//.          
+          .//-..        ..-//.          
+          ./////        /////.          
+                                        
+--------       \`.------.\`       --------
+-----://     .:///::::///:.     //:-----
+     -//   \`://-\`      \`-//:\`   //-     
+           ://\`          \`//:           
+          \`//-            -//\`          
+          \`//-            -//\`          
+           ://\`          \`//:           
+     -//   \`://-\`      \`-//:\`   //-     
+-----://     .:///::::///:.     //:-----
+--------       \`.------.\`       --------
+                                        
+          ./////        /////.          
+          .//-..        ..-//.          
+          .//.            .//.          
+          .//.            .//.
+`;
+    console.log(logo);
+    console.log('Oi! What you doin pokin round ere?');
+})

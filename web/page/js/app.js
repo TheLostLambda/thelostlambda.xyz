02553a22c3ce01a7756854aa4c39bf1367dc2e5a
Brooks J Rady <b.j.rady@gmail.com>
2019-07-23 23:36:58 +0100

    Fix go home bug and work on typewriter

--- web/page/js/app.js
+++ web/page/js/app.js
@@ -30,27 +30,36 @@ document.addEventListener('DOMContentLoaded', function(event) {
     // I should add some checking for if these apply to the page being loaded
     showSplash();
     setInterval(showSplash, 5000);
-    document.querySelector('.navbar > a').setAttribute('href', 'javascript:');
+    if (window.matchMedia('(max-width: 720px)').matches) {
+        document.querySelector('.navbar > a').setAttribute('href', 'javascript:');
+    }
     document.getElementById('links').addEventListener("transitionend", awaitDropdown, true);
 })
 
 function showSplash() {
     var splashList = document.querySelectorAll('#splashes > p:not(.show)');
-    var shown = document.querySelector('.show');
-    if (shown != null) {
-        shown.classList.remove('show');
-    }
-    var splash = splashList[Math.floor(Math.random() * splashList.length)];
-    if (splash != null) {
-        splash.classList.add('show');
+    var splash = splashList[Math.floor(Math.random() * splashList.length)].textContent;
+    var writer = document.querySelector('#splashes');
+    // Clear splashes
+    writer.querySelectorAll('span').forEach((spn) => {
+        writer.removeChild(spn);
+    })
+    // Add splash
+    for (var i = 0; i < splash.length; i++) {
+        var span = document.createElement('span');
+        var text = document.createTextNode(splash.charAt(i));
+        span.appendChild(text);
+        writer.appendChild(span);
     }
 }
 
 function awaitDropdown() {
-    var linkHome = document.querySelector('.navbar > a');
-    if (linkHome.getAttribute('href') === '/') {
-        linkHome.setAttribute('href', 'javascript:');
-    } else {
-        linkHome.setAttribute('href', '/')
+    if (window.matchMedia('(max-width: 720px)').matches) {
+        var linkHome = document.querySelector('.navbar > a');
+        if (linkHome.getAttribute('href') === '/') {
+            linkHome.setAttribute('href', 'javascript:');
+        } else {
+            linkHome.setAttribute('href', '/')
+        }
     }
 }
